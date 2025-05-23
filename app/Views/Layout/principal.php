3e7c7b4ded6002a5cd37ce3be553d1dac5271fed
Flor Cocomarola <ingeelect92@gmail.com>
2025-05-23 18:27:43 -0300

    Actualización plantilla

--- app/Views/Layout/principal.php
+++ app/Views/Layout/principal.php
@@ -12,21 +12,30 @@
     <header class="row-1">
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                 <span class="navbar-toggler-icon"></span>
+                <a class="navbar-brand" href="#"><img src="<?= base_url('img/logo.png') ?>" alt="Logo" class="logo"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>      
+                 
                 </button>
              <div class="collapse navbar-collapse " id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                      <li class="nav-item">
-                         <a class="nav-link active" aria-current="page" href="#">Home</a>
+                         <a class="nav-link active" aria-current="page" href="#">Principal</a>
                     </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">Quiénes somos</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Acerca de</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Registrarse</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Login</a>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                             Dropdown
+                            Productos
                         </a>
                             <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="#">Action</a></li>
@@ -35,13 +44,13 @@
                                 <li><a class="dropdown-item" href="#">Something else here</a></li>
                             </ul>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
+                    
                 </ul>
                 <form class="d-flex" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button class="btn btn-outline-success" type="submit">Search</button>
+                    <button class="btn btn-outline-light" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"      fill="currentColor" class="bi bi-search" viewBox="0 0 15 15">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                    </svg></button>
                  </form>
                 </div>
             </div>
@@ -51,13 +60,13 @@
         <?= $this->renderSection('content') ?>
     </main>
     <footer class="row-1 fixed-bottom">
-    <div class="container">
-        <p>&copy; 2023 Your Company Name. All rights reserved.</p>
-        <p>Follow us on:
-            <a href="https://www.facebook.com" target="_blank">Facebook</a>,
-            <a href="https://www.twitter.com" target="_blank">Twitter</a>,
-            <a href="https://www.instagram.com" target="_blank">Instagram</a>
-        </p>
+        <div class="container">
+            <div>
+                <a href="https://www.facebook.com" target="_blank"><img src="<?= base_url('img/facebook.png') ?>" alt="Facebook"></a>
+                <a href="https://x.com" target="_blank"><img src="<?= base_url('img/x.png') ?>" alt="X"></a>
+                <a href="https://www.instagram.com" target="_blank"><img src="<?= base_url('img/ig.png') ?>" alt="Instagram"></a>
+            </div>
+            <div>&copy; 2023 Your Company Name. All rights reserved.</div>
         </div>
     </footer>
     <script src="<?= base_url('js/bootstrap.bundle.min.js') ?>"></script>
